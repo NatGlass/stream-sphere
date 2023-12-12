410f4d4efb5f6b57fa8193c8a0ccce735ef89e83
Visibyte <nathanglass26@gmail.com>
2023-12-12 16:46:35 +0000

    hint atom

---
+++ src/components/atoms/hint/index.tsx
@@ -0,0 +1,33 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '../tooltip';
+
+type THintProps = {
+  label: string;
+  children: React.ReactNode;
+  asChild?: boolean;
+  side?: 'top' | 'bottom' | 'left' | 'right';
+  align?: 'start' | 'center' | 'end';
+};
+
+function Hint({ label, children, asChild, side, align }: THintProps) {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={0}>
+        <TooltipTrigger asChild={asChild}>{children}</TooltipTrigger>
+        <TooltipContent
+          className="text-white bg-background"
+          side={side}
+          align={align}
+        >
+          <p className="font-semibold">{label}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
+
+export default Hint;
