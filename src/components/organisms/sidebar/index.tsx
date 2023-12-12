dd7ffc07cdf273bd0d054b5d161bb6a37ac1b308
Visibyte <nathanglass26@gmail.com>
2023-12-12 18:48:00 +0000

    render recommended

--- src/components/organisms/sidebar/index.tsx
+++ src/components/organisms/sidebar/index.tsx
@@ -1,10 +1,18 @@
-import Wrapper from "@/components/molecules/sidebar-wrapper";
-import ToggleSidebar from "@/components/molecules/toggle-sidebar";
+import Recommended from '@/components/molecules/sidebar-recommended';
+import Wrapper from '@/components/molecules/sidebar-wrapper';
+import ToggleSidebar from '@/components/molecules/toggle-sidebar';
+import getRecommended from '@/lib/recommended-service';
 
-function Sidebar() {
-  return <Wrapper>
-    <ToggleSidebar />
-  </Wrapper>;
+async function Sidebar() {
+  const recommended = await getRecommended();
+  return (
+    <Wrapper>
+      <ToggleSidebar />
+      <div className="pt-4 space-y-4 lg:pt-0">
+        <Recommended data={recommended} />
+      </div>
+    </Wrapper>
+  );
 }
 
 export default Sidebar;
