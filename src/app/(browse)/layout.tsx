2302f9743041cf208382984556ca8450074326a9
Visibyte <nathanglass26@gmail.com>
2023-12-12 15:29:36 +0000

    browse layout

---
+++ src/app/(browse)/layout.tsx
@@ -0,0 +1,14 @@
+import Navbar from '@/components/organisms/navbar';
+import { PropsWithChildren } from 'react';
+
+function BrowseLayout({ children }: PropsWithChildren) {
+  return (
+    <>
+      <Navbar />
+      <div className="flex h-full pt-20">{children}</div>
+    </>
+  );
+}
+
+export default BrowseLayout;
+ 
