ae9ad95a32e8da6dd3bed30bd46659d76173e7a8
Visibyte <nathanglass26@gmail.com>
2023-12-13 12:03:59 +0000

    follow service

--- src/lib/follow-service.ts
+++ src/lib/follow-service.ts
@@ -29,3 +29,43 @@ export default async function isFollower(id: string) {
     return false;
   }
 }
+
+export async function followUser(id: string) {
+  const currentUser = await getUser();
+
+  const otherUser = await prisma.user.findUnique({
+    where: { id },
+  });
+
+  if (!otherUser) {
+    throw new Error('User not found');
+  }
+
+  if (otherUser.id === currentUser.id) {
+    throw new Error('Cannot follow yourself');
+  }
+
+  const existingFollow = await prisma.follow.findFirst({
+    where: {
+      followerId: currentUser.id,
+      followingId: otherUser.id,
+    },
+  });
+
+  if (existingFollow) {
+    throw new Error('Already following user');
+  }
+
+  const follow = await prisma.follow.create({
+    data: {
+      followerId: currentUser.id,
+      followingId: otherUser.id,
+    },
+    include: {
+      following: true,
+      follower: true,
+    },
+  });
+
+  return follow;
+}
