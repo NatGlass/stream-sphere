e4c94130ff7c0ef81ff3eb880f88d789c1a0049d
Visibyte <nathanglass26@gmail.com>
2023-12-13 10:21:20 +0000

    exclude self from recommendations

--- src/lib/recommended-service.ts
+++ src/lib/recommended-service.ts
@@ -1,12 +1,39 @@
-import prisma from "./db";
+import getUser from './auth-service';
+import prisma from './db';
 
 async function getRecommended() {
-  const users = await prisma.user.findMany({
-    orderBy: {
-      createdAt: 'desc',
-    },
-  });
+  let userId;
 
+  try {
+    const currentUser = await getUser();
+    userId = currentUser.id;
+  } catch (error) {
+    userId = null;
+  }
+
+  let users = [];
+
+  if (userId) {
+    // Logged in user
+    users = await prisma.user.findMany({
+      where: {
+        // Exclude the current user in the results
+        NOT: {
+          id: userId,
+        },
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+  } else {
+    // Public user
+    users = await prisma.user.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+  }
   return users;
 }
 
