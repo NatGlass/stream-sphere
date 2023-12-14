afbf3ca792e41fbf3170e7809a19bbf2d5e5d8f1
Visibyte <nathanglass26@gmail.com>
2023-12-14 17:08:23 +0000

    add hint to copy to clipboard button

--- src/components/molecules/copy-button/index.tsx
+++ src/components/molecules/copy-button/index.tsx
@@ -1,29 +1,34 @@
 'use client';
 
-import { Button } from "@/components/atoms/button";
-import { CheckCheck, Copy } from "lucide-react";
-import { useState } from "react";
+import { Button } from '@/components/atoms/button';
+import Hint from '@/components/atoms/hint';
+import { CheckCheck, Copy } from 'lucide-react';
+import { useState } from 'react';
 
 type TCopyButton = {
-    value?: string;
-}
+  value?: string;
+};
 
 function CopyButton({ value }: TCopyButton) {
-    const [isCopied, setIsCopied] = useState(false);
+  const [isCopied, setIsCopied] = useState(false);
 
-    const onCopy = () => {
-        if (!value) return
+  const onCopy = () => {
+    if (!value) return;
 
-        setIsCopied(true);
-        navigator.clipboard.writeText(value);
-        setTimeout(() => setIsCopied(true), 2000);
-    }
+    setIsCopied(true);
+    navigator.clipboard.writeText(value);
+    setTimeout(() => setIsCopied(true), 2000);
+  };
 
-    const Icon = isCopied ? CheckCheck : Copy;
+  const Icon = isCopied ? CheckCheck : Copy;
 
-    return <Button onClick={onCopy} disabled={!value || isCopied} variant="ghost">
-      <Icon className="w-4 h-4 text-white" />
-  </Button>;
+  return (
+    <Hint label="Copy to clipboard" asChild>
+      <Button onClick={onCopy} disabled={!value || isCopied} variant="ghost">
+        <Icon className="w-4 h-4 text-white" />
+      </Button>
+    </Hint>
+  );
 }
 
 export default CopyButton;
