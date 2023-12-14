596ffb2f9466ae51cd73d88571d20709324d4624
Visibyte <nathanglass26@gmail.com>
2023-12-14 11:49:35 +0000

    finish dashboard sidebar

--- src/components/molecules/dashboard/dashboard-sidebar-navigation/index.tsx
+++ src/components/molecules/dashboard/dashboard-sidebar-navigation/index.tsx
@@ -1,5 +1,63 @@
+'use client';
+
+import { useUser } from '@clerk/nextjs';
+import { Fullscreen, KeyRound, MessageSquare, Users } from 'lucide-react';
+import { usePathname } from 'next/navigation';
+import DashboardNavItem, {
+  DashboardNavItemSkeleton,
+} from '../dashboard-nav-item';
+
 function DashboardSidebarNavigation() {
-  return <div>DashboardSidebarNavigation</div>;
+  const pathname = usePathname();
+  const { user } = useUser();
+
+  const routes = [
+    {
+      label: 'Stream',
+      href: `/user/${user?.username}`,
+      icon: Fullscreen,
+    },
+    {
+      label: 'Keys',
+      href: `/user/${user?.username}/keys`,
+      icon: KeyRound,
+    },
+    {
+      label: 'Chat',
+      href: `/user/${user?.username}/chat`,
+      icon: MessageSquare,
+    },
+    {
+      label: 'Community',
+      href: `/user/${user?.username}/community`,
+      icon: Users,
+    },
+  ];
+
+  if (!user?.username) {
+    return (
+      <ul className="space-y-2">
+        {[...Array(4)].map((_, i) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <DashboardNavItemSkeleton key={i} />
+        ))}
+      </ul>
+    );
+  }
+
+  return (
+    <ul className="space-y-2 px-2 pt-4 lg:pt-0">
+      {routes.map((route) => (
+        <DashboardNavItem
+          key={route.href}
+          label={route.label}
+          icon={route.icon}
+          href={route.href}
+          isActive={pathname === route.href}
+        />
+      ))}
+    </ul>
+  );
 }
 
 export default DashboardSidebarNavigation;
--- src/components/organisms/dashboard-sidebar/index.tsx
+++ src/components/organisms/dashboard-sidebar/index.tsx
@@ -1,9 +1,11 @@
+import DashboardSidebarNavigation from "@/components/molecules/dashboard/dashboard-sidebar-navigation";
 import DashboardSidebarWrapper from "@/components/molecules/dashboard/dashboard-sidebar-wrapper";
 import ToggleDashboardSidebar from "@/components/molecules/dashboard/toggle-dashboard-sidebar";
 
 function DashboardSidebar() {
   return <DashboardSidebarWrapper>
     <ToggleDashboardSidebar />
+    <DashboardSidebarNavigation />
   </DashboardSidebarWrapper>;
 }
 
