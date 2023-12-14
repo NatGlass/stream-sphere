8e66a9cd62a1196cf12488b2654a07d9894980d8
Visibyte <nathanglass26@gmail.com>
2023-12-14 16:55:49 +0000

    ingress service

---
+++ src/actions/ingress.ts
@@ -0,0 +1,86 @@
+'use server';
+
+import getUser from '@/lib/auth-service';
+import prisma from '@/lib/db';
+import {
+  CreateIngressOptions,
+  IngressAudioEncodingPreset,
+  IngressClient,
+  IngressInput,
+  IngressVideoEncodingPreset,
+  RoomServiceClient,
+} from 'livekit-server-sdk';
+import { TrackSource } from 'livekit-server-sdk/dist/proto/livekit_models';
+import { revalidatePath } from 'next/cache';
+
+const roomService = new RoomServiceClient(
+  process.env.LIVEKIT_API_URL!,
+  process.env.LIVEKIT_API_KEY!,
+  process.env.LIVEKIT_API_SECRET!
+);
+
+const ingressClient = new IngressClient(process.env.LIVEKIT_API_URL!);
+
+export async function resetIngress(hostIdentity: string) {
+  const ingresses = await ingressClient.listIngress({
+    roomName: hostIdentity,
+  });
+
+  const rooms = await roomService.listRooms([hostIdentity]);
+
+  const deleteRoomPromises = rooms.map((room) =>
+    roomService.deleteRoom(room.name)
+  );
+
+  await Promise.all(deleteRoomPromises);
+
+  const deleteIngressPromises = ingresses
+    .filter((ingress): ingress is {ingressId: string} => ingress.ingressId !== undefined)
+    .map((ingress) => ingressClient.deleteIngress(ingress.ingressId));
+
+  await Promise.all(deleteIngressPromises);
+}
+
+export default async function createIngress(ingressType: IngressInput) {
+  const currentUser = await getUser();
+
+  const options: CreateIngressOptions = {
+    name: currentUser.username,
+    roomName: currentUser.id,
+    participantName: currentUser.username,
+    participantIdentity: currentUser.id,
+  };
+
+  if (ingressType === IngressInput.WHIP_INPUT) {
+    options.bypassTranscoding = true;
+  } else {
+    options.video = {
+      source: TrackSource.CAMERA,
+      preset: IngressVideoEncodingPreset.H264_1080P_30FPS_3_LAYERS,
+    };
+    options.audio = {
+      source: TrackSource.MICROPHONE,
+      preset: IngressAudioEncodingPreset.OPUS_STEREO_96KBPS,
+    };
+  }
+
+  const ingress = await ingressClient.createIngress(ingressType, options);
+
+  if (!ingress || !ingress.url || !ingress.streamKey) {
+    throw new Error('failed to create ingress');
+  }
+
+  await prisma.stream.update({
+    where: {
+      userId: currentUser.id,
+    },
+    data: {
+      ingressId: ingress.ingressId,
+      serverUrl: ingress.url,
+      streamKey: ingress.streamKey,
+    },
+  });
+
+  revalidatePath(`/users/${currentUser.username}/keys`);
+  return ingress;
+}
