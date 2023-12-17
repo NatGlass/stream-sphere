dde970deea927dfa3a79df6d1e6d09f3622d29c6
Visibyte <nathanglass26@gmail.com>
2023-12-17 16:03:36 +0000

    remove comments

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,20 +1,11 @@
-// import {
-//   currentUser as clerkCurrentUser,
-//   useAuth as useClerkAuth,
-//   useUser as useClerkUser,
-// } from '@clerk/nextjs';
-
 export function useAuth() {
-//   const auth = useClerkAuth();
   return null;
 }
 
 export function useUser() {
-//   const user = useClerkUser();
   return null;
 }
 
 export async function currentUser() {
-//   const user = await clerkCurrentUser();
   return null;
 }
